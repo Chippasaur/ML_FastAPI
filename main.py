31d5ad74501dc54ebaff48c58976e41514217b9a
Chippasaur <alvinchungg@gmail.com>
2020-03-29 14:48:20 +0800

    added first route

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import uvicorn
+from fastapi import FastAPI
+
+# init app
+app = FastAPI()
 
 
-print("Hello World")
+# Routes
+@app.get('/')
+async def index():
+    return {"text": "Hello Worlds"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
